5aeba45754be827dc62e822b9c8500cd040a271d
Victor Salinas <victorsalinas6@gmail.com>
2024-01-12 13:25:56 -0600

    Finished the version 1, next time will try to implement an SQL database with Dapper

--- ToDoApp/ToDoApp.cs
+++ ToDoApp/ToDoApp.cs
@@ -14,6 +14,7 @@ namespace ToDoApp
             Console.ForegroundColor = ConsoleColor.Blue;
             Menus.StartUp();
             var on = true;
+
             do
             {
                 
